cb20ceed05d5feee67d045aabc258d081cc99b42
ADX <90461352+ADXServers@users.noreply.github.com>
2021-09-20 17:46:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,345 @@
+#!/usr/bin/env python3
+import sys
+import audioop
+import os
+import time
+from typing import Mapping
+import git
+from git import Repo
+from neuralintents import GenericAssistant
+import speech_recognition
+import pyttsx3 as tts
+import sys
+import os
+import wikipedia
+#from voiceAI import *
+from voiceapi import *
+#reco = speech_recognition.Recognizer()
+#speaker = tts.init("espeak")
+#voices = speaker.getProperty('voices')
+#speaker.setProperty('voice',voices[11].id)
+print("\033c", end="")
+print("This Commandline was made by ADX")
+print("Discord - ADX#0001")
+print("Website - https://adxservers.xyz")
+print("Commands list:\nLogout = logs out of the current user.\nvoiceAI = starts a voice assistant.\ncdir = Creates a directory\ncls = Clears all text from console.\nhelp = Lists all commands\ncreateuser = Creates a user\npasswd = Changes password of a user\nchangename = Changes the name of an user\nls = Lists all files inside a directory")
+print("Warning! EVERYTHING IS CASE SENSITIVE.")
+
+
+
+file = open("users/root.txt", "a")
+devmode = False
+usernamefound = False
+passwordfound = False
+loggedin = False
+#SaveLogin = None
+cmd = None
+
+#Functions
+def cls():
+    print("\033c", end="")
+
+
+
+#Commands ( DO NOT EDIT )
+
+#Clear command
+
+def clscmd():
+    print("Clearing commandline")
+    time.sleep(1)
+    cls()
+    print("Commandline cleared")
+    cmdline()
+    return
+#if commandline == "Yes":
+    #if cmd == "cls":
+        #clscmd()
+#/Clear command
+
+#CDIR (Create Folder)
+
+def cmdcdir():
+    directory = input("Directory Name: ")
+    dirpath = input("Directory Path: ")
+    path = os.path.join(dirpath, directory)
+    os.mkdir(path)
+    print("Directory '%s' created" %directory)
+    cmdline()
+    return
+#if commandline == "Yes":
+    #if cmd == "cdir":
+        #cmdcdir()
+#/CDIR (Create Folder)
+
+#HELP (Lists all commands)
+
+def help():
+    cls()
+    print("This Commandline was made by ADX")
+    print("Discord - ADX#0001")
+    print("Website - https://adxservers.xyz")
+    print("Commands list:\nLogout = logs out of the current user.\nvoiceAI = starts a voice assistant.\ncdir = Creates a directory\ncls = Clears all text from console.\nhelp = Lists all commands\ncreateuser = Creates a user\npasswd = Changes password of a user\nchangename = Changes the name of an user\nls = Lists all files inside a directory")
+    cmdline()
+    return
+#if commandline == "Yes":
+    #if cmd == "help":
+        #help()
+#/HELP (Lists all commands)
+
+#Createuser (Creates user)
+def createuser():
+    arg1 = input("Username: ")
+    if os.path.exists("users/"+arg1+".txt"):
+        print(arg1+" already exists!")
+        return
+    else:
+        file = open("users/"+arg1+".txt", "a")
+        file.close()
+        time.sleep(0.3)
+        arg2 = input("Enter password: ")
+        file = open("users/"+arg1+".txt", "w")
+        file.write(arg2+"\n")
+        time.sleep(0.5)
+        cls()
+        print("New user has been made!\nUsername: "+arg1+"\nPassword: "+arg2)
+        return
+#/Createuser (Creates user)
+
+#Passwd (Changes password of an user)
+def cpasswd():
+    arg1 = input("User: ")
+    if os.path.exists("users/"+arg1+".txt"):
+        arg2 = input("New password: ")
+        arg3 = input("Confirm password: ")
+        if arg2 == arg3:
+            file = open("users/"+arg1+".txt", "w")
+            file.write(arg2)
+            cls()
+            print("Password Changed!\nUsername: "+arg1+"\nPassword: "+arg2)
+            return
+        else:
+            print("Failed to confirm password.")
+            return
+#/Passwd (Changes password of an user)
+
+#changename (changes name of an user)
+def cname():
+    arg1 = input("Current username: ")
+    if os.path.exists("users/"+arg1+".txt"):
+        arg2 = input("New name: ")
+        arg3 = input("Confirm name: ")
+        if arg2 == arg3:
+            arg4 = "/users/"+arg1+".txt"
+            arg5 = "/users/"+arg2+".txt"
+            os.rename(arg4, arg5)
+            print("Name changed!\nUsername: "+arg2)
+            return
+        else:
+            print("Failed to confirm name!")
+            return
+#/changename (changes name of an user)
+
+#Listdir
+def listdir():
+    arg1 = input("Directory [-C = Current directory]: ")
+    if arg1 == "-C":
+        start_path = '.'
+        os.listdir(start_path)
+        print(os.listdir(start_path))
+        return
+    elif os.path.exists(arg1):
+        print(os.listdir(arg1))
+        return
+    else:
+        print("Directory not found.")
+        return
+#/Listdir
+
+#Logout
+def logoutf():
+    arg2 = input("Is your login saved? [Yes / No]: ")
+    if arg2 == "Yes":
+        SaveLogin = "No"
+        os.remove("users/latest.txt")
+        cls()
+        print("Succesfully removed your saved login.")
+        loggedin = False
+        commandline = False
+        print("\nThis command line will shutdown in 5 seconds!\n")
+        time.sleep(1)
+        print("\nThis command line will shutdown in 4 seconds!\n")
+        time.sleep(1)
+        print("\nThis command line will shutdown in 3 seconds!\n")
+        time.sleep(1)
+        print("\nThis command line will shutdown in 2 seconds!\n")
+        time.sleep(1)
+        print("\nThis command line will shutdown in 1 seconds!\n")
+        time.sleep(1)
+        print("\nGoodbye!\n")
+        time.sleep(0.3)
+        exit()
+    else:
+        arg1 = input("Write your password confirm logout: ")
+        file = open("users/"+name+".txt", "r")
+        if arg1 in file.read():
+            cls()
+            print("Succesfully logged out of your account.\n")
+            print("\nThis command line will shutdown in 5 seconds!\n")
+            loggedin = False
+            commandline = False
+            time.sleep(1)
+            print("\nThis command line will shutdown in 4 seconds!\n")
+            time.sleep(1)
+            print("\nThis command line will shutdown in 3 seconds!\n")
+            time.sleep(1)
+            print("\nThis command line will shutdown in 2 seconds!\n")
+            time.sleep(1)
+            print("\nThis command line will shutdown in 1 seconds!\n")
+            time.sleep(1)
+            print("\nGoodbye!\n")
+            time.sleep(0.3)
+            exit()
+#/Logout
+
+def gAI():
+    cls()
+    reco = speech_recognition.Recognizer()
+    speaker = tts.init("espeak")
+    voices = speaker.getProperty('voices')
+    speaker.setProperty('voice',voices[3].id)
+    speaker.setProperty("rate", 178)
+    speaker.setProperty('volume', 0.2)
+    g = True
+    commandline = "No"
+    #print("debug")
+    file = open("users/current.txt", "w")
+    file.write(name)
+    file.close()
+    mainf()
+    
+
+def cmdline():
+    while True:
+        cmd = input("Enter Command: ")
+
+        if commandline == "Yes":
+            if cmd == "cls":
+                clscmd()
+        if commandline == "Yes":
+            if cmd == "cdir":
+                cmdcdir()
+        if commandline == "Yes":
+            if cmd == "voiceAI":
+                gAI()
+        if commandline == "Yes":
+            if cmd == "help":
+                help()
+        if commandline == "Yes":
+            if cmd == "createuser":
+                createuser()
+        if commandline == "Yes":
+            if cmd == "passwd":
+                cpasswd()
+        if commandline == "Yes":
+            if cmd == "changename":
+                cname()
+        if commandline == "Yes":
+            if cmd == "ls":
+                listdir()
+        if commandline == "Yes":
+            if cmd == "logout":
+                logoutf()
+        if commandline == "Yes": 
+           if cmd == "quit":
+                exit()
+
+    
+
+
+#ROOT User
+file = open("users/root.txt", "r")
+#if "root" in file.readlines():
+    #print("ROOT user found")
+#else:
+    #file = open("users/root.txt", "w")
+    #file.write("root\n")
+    #file.close()
+    #file = open("users/root.txt", "r")
+if "admin" in file.read():
+    print("ROOT user found")
+else:
+    file = open("users/root.txt", "w")
+    file.write("admin\n")
+    file.close()
+
+
+#Developer mode
+if devmode == True:
+    debug = input("debug: ")
+    if debug == "dev":
+        file = open("users/adx.txt", "w")
+        file.write("adx\nadxvps")
+        file.close()
+
+
+
+#User management
+
+if os.path.exists("users/latest.txt"):
+        file.close()
+        file = open("users/latest.txt", "r")
+        print("Logged in as "+file.read())
+        loggedin = True
+else:
+    name = input("Enter username:")
+    if os.path.exists("users/"+name+".txt"):
+        file = open("users/"+name+".txt", "r")
+        file.close()
+        usernamefound = True
+        usernameid = name
+        cls()
+        print("Username " + usernameid + " found!")
+
+    else:  
+        print("Username not found! Exiting program") #file = open("users/"+createuser+".txt", "w")
+        exit()
+
+if usernamefound == True:
+    passwd = input("Enter password:")
+    file = open("users/"+name+".txt", "r")
+    if passwd in file.read():
+        cls()
+        print("Logged in as " + name)
+        loggedin = True
+        SaveLogin = input("Do you want to save this login?\n [ Yes / No ]: ")
+        if SaveLogin == "Yes":
+            file.close()
+            file = open("users/latest.txt", "a")
+            file.close
+            file = open("users/latest.txt", "w")
+            file.write(name)
+            
+
+
+if loggedin == True:
+    commandline = input("Do you want to start commandline? Yes/No:\n")
+    if commandline == "Yes":
+        cls()
+        print("Opening commandline.")
+        time.sleep(1)
+        cls()
+        print("This Commandline was made by ADX")
+        print("Discord - ADX#0001")
+        print("Website - https://adxservers.xyz")
+        print("Commands list:\nLogout = logs out of the current user.\nvoiceAI = starts a voice assistant.\ncdir = Creates a directory\ncls = Clears all text from console.\nhelp = Lists all commands\ncreateuser = Creates a user\npasswd = Changes password of a user\nchangename = Changes the name of an user\nls = Lists all files inside a directory")
+        cmdline()
+    else:
+        print("Logout Yes/No") #logout function
+
+
+
+
+
+
+
---
+++ voiceapi.py
@@ -0,0 +1,178 @@
+import sys
+import audioop
+import os
+import time
+from typing import Mapping
+import git
+from git import Repo
+from neuralintents import GenericAssistant
+import speech_recognition
+import pyttsx3 as tts
+import sys
+import os
+import wikipedia
+
+def mainf():
+    def cls():
+        print("\033c", end="")
+    #speaker = tts.init('dummy')
+    wname = "default"
+    reco = speech_recognition.Recognizer()
+    speaker = tts.init("espeak")
+    voices = speaker.getProperty('voices')
+    speaker.setProperty('voice',voices[3].id)
+    speaker.setProperty("rate", 160)
+    speaker.setProperty('volume', 0.2)
+    speaker.say("Welcome "+wname)
+    speaker.runAndWait()
+    cls()
+    print("Welcome "+wname)
+    speaker.say("This Voice Assistant was made by ADX")
+    speaker.runAndWait()
+    print("This Voice Assistant was made by ADX")
+    print("Discord - ADX#0001")
+    print("Website - https://adxservers.xyz")
+    print("Voice Command list:\nWhat is my name = Says your name.\nChange my name = Changes your name.\nHello = Responds with a greeting\nCreate note = Creates a note\nWho is / What is / Wikipedia  = Finds information from wikipedia\nBye = Stops program\n")
+    def speak(text):
+        speaker.say(text)
+        speaker.runAndWait
+    def Hi():
+        speaker.say("Hello")
+        print("Hello")
+        speaker.runAndWait()
+
+    def bye():
+        speaker.say("Goodbye!")
+        speaker.runAndWait()
+        sys.exit()
+    def thx():
+        speaker.say("Hello.")
+        speaker.runAndWait()
+    def create_note():
+        global reco
+        speaker.say("What do you want to write in your notes")
+        speaker.runAndWait()
+
+    
+
+        
+
+        d = False
+        while not d:
+            try:
+                with speech_recognition.Microphone() as mic:
+                    reco = speech_recognition.Recognizer()
+                    reco.adjust_for_ambient_noise(mic, duration=0.2)
+                    audio = reco.listen(mic)
+                    note = reco.recognize_google(audio)
+                    note = note.lower()
+                    print(note)
+                    speaker.say("What do you want to name your file as")
+                    speaker.runAndWait()
+                    reco.adjust_for_ambient_noise(mic, duration=0.2)
+                    audio = reco.listen(mic)
+                    fname = reco.recognize_google(audio)
+                    fname = fname.lower()
+                    print(fname)
+                with open(fname, "w") as f:
+                    f.write(note)
+                    d = True
+                    speaker.say(f"Your note has been written, name of your note is {fname}")
+                    speaker.runAndWait()
+            except speech_recognition.UnknownValueError:
+                reco = speech_recognition.Recognizer()
+                speaker.say("I did not understand.")
+                speaker.runAndWait()
+
+    def wiki():
+        global reco
+        speaker.say("What do you want to search from wikipedia")
+        speaker.runAndWait()
+        d = False
+        while not d:
+            try:
+                with speech_recognition.Microphone() as mic:
+                    reco = speech_recognition.Recognizer()
+                    reco.adjust_for_ambient_noise(mic, duration=0.2)
+                    audio = reco.listen(mic)
+                    winfo = reco.recognize_google(audio)
+                    winfo = winfo.lower()
+                    print(winfo)
+                    d = True
+                    speaker.say(wikipedia.summary(winfo))
+                    print(wikipedia.summary(winfo))
+                    speaker.runAndWait()
+            except speech_recognition.UnknownValueError:
+                reco = speech_recognition.Recognizer()
+                print(winfo)
+                speaker.say("Wikipedia page not found. Try again")
+                print("Wikipedia page not found. Try again")
+                speaker.runAndWait()
+    def setname():
+        global reco
+        speaker.say("What do you want to set as your name")
+        speaker.runAndWait()
+        d = False
+        while not d:
+            try:
+                with speech_recognition.Microphone() as mic:
+                    reco = speech_recognition.Recognizer()
+                    reco.adjust_for_ambient_noise(mic, duration=0.2)
+                    audio = reco.listen(mic)
+                    wname = reco.recognize_google(audio)
+                    wname = wname.lower()
+                    file = open("users/current.txt", "w")
+                    file.write(wname)
+                    file.close()
+                    print(wname+" Is now your new name")
+                    d = True
+                    speaker.say("Hello "+wname)
+                    speaker.runAndWait()
+            except speech_recognition.UnknownValueError:
+                reco = speech_recognition.Recognizer()
+                speaker.say("I did not understand.")
+                speaker.runAndWait()
+
+    def myname():
+        file = open("users/current.txt", "r")
+        wname = file.read()
+        speaker.say("Your name is "+wname)
+        speaker.runAndWait
+
+
+
+    mappings = {
+        'greeting' : Hi,
+        'create_note' : create_note,
+        'goodbye' : bye,
+        'thanks' : thx,
+        'wiki' : wiki,
+        'setname' : setname,
+        'name' : myname
+    }
+    AI = GenericAssistant('intents.json', intent_methods=mappings)
+    #AI.train_model()
+    AI.load_model()
+
+
+
+    while True:
+        try:
+            with speech_recognition.Microphone() as mic:
+                cls()
+                print("This Voice Assistant was made by ADX")
+                print("Discord - ADX#0001")
+                print("Website - https://adxservers.xyz")
+                print("Voice Command list:\nWhat is my name = Says your name.\nChange my name = Changes your name.\nHello = Responds with a greeting\nCreate note = Creates a note\nWho is / What is / Wikipedia  = Finds information from wikipedia\nBye = Stops program\n")
+                speaker.runAndWait()
+                reco.adjust_for_ambient_noise(mic, duration=0.5)
+                audio = reco.listen(mic)
+                message = reco.recognize_google(audio)
+                message = message.lower()
+                print(message)
+                AI.request(message)
+        except speech_recognition.UnknownValueError:
+                speaker.say("Voice command not found. Try again")
+                #speak("I didnt understand")
+                print("Voice command not found. Try again")
+                speaker.runAndWait()
